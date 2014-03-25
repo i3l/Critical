595eea7abde0a18985ef12d239e0f7f0294a4bac
bmenn <bryant.menn@gmail.com>
2014-03-25 02:03:44 -0400

    Corrected database errors

--- scripts/txtDataToSqlite.py
+++ scripts/txtDataToSqlite.py
@@ -7,29 +7,46 @@ con = None
 
 data = np.genfromtxt(fname=fname, skip_header=1, delimiter=',',
                      dtype=None)
-patid = data[0][2]
-age = data[1][2]
-gender = data[2][2]
-height = data[3][2]
-icuType = data[4][2]
-weight = data[5][2]
 
+# Patient descriptors go into the pinfo table. Patient descriptors denoted
+# by timestamp of 00:00 and parameter names:
+#       RecordID
+#       Age
+#       Gender
+#       Height
+#       ICUType
+#       Weight
 try:
-    con = sqlite.connect('../data/data.db')
+    con = sqlite.connect('data/data.db')
 
     with con:
         cur = con.cursor()
 
-        cmdstr = "INSERT INTO pinfo VALUES(%d, %d, %d, %d, %d, %d);" \
-                % (patid, age, gender, height, icuType, weight)
-        # print cmdstr
-        cur.execute(cmdstr)
+        for (t, p, v) in data:
+            if (p == 'RecordID'):
+                patid = v
+            elif (p == 'Age'):
+                age = v
+            elif (p == 'Gender'):
+                gender = v
+            elif (p == 'Height'):
+                height = v
+            elif (p == 'ICUType'):
+                icuType = v
+            elif (p == 'Weight'):
+                weight = v
+            else:
+                # Assuming we already have assigned RecordID to patid
+                cmdstr = ('INSERT INTO vstats VALUES('
+                          + str(patid) + ', "' + t + '", "'
+                          + p + '", ' + str(v) + ');')
+                cur.execute(cmdstr)
 
-        for (t, p, v) in data[6:]:
-            cmdstr = "INSERT INTO vstats VALUES(%d, '%s', '%s', %f);" % \
-                    (patid, t, p, v)
-            # print cmdstr
-            cur.execute(cmdstr)
+        cmdstr = ('INSERT INTO pinfo VALUES('
+                  + str(patid) + ', ' + str(age) + ', ' + str(gender) + ', '
+                  + str(height) + ', ' + str(icuType) + ', '
+                  + str(weight) + ');')
+        cur.execute(cmdstr)
 
 except sqlite.Error, e:
     print "Error %s:" % e.args[0]
